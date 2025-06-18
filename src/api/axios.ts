145b7e05c31b631571cc792e5a89a740d57546cc
Dmytro Khabaznia <Dmytro.Khabaznia@playtech.com>
2025-06-18 11:56:32 +0300

    Small messages refactor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,24 +1,30 @@
 import axios, { AxiosInstance } from 'axios';
 import { AppConfig } from '../common/configuration';
 import { ILogger } from '../common/types/logger';
+import {
+	BIN_API_REQUEST_ERROR,
+	BIN_API_REQUEST_SENT,
+	BIN_API_RESPONSE_ERROR,
+	BIN_API_RESPONSE_RECEIVED
+} from './messages';
 
 export const getBinApiAxiosInstance = (appConfig: AppConfig, logger: ILogger): AxiosInstance => {
 	const instance = axios.create({ baseURL: appConfig.binApi.url })
 
 	instance.interceptors.request.use((config) => {
 		config.headers['x-api-key'] = appConfig.binApi.apiKey;
-		logger.debug('Sent a request BIN API')
+		logger.debug(BIN_API_REQUEST_SENT)
 		return config;
 	}, (error: Error) => {
-		logger.error('Error sending request to BIN API', { error })
+		logger.error(BIN_API_REQUEST_ERROR, { error })
 		return Promise.reject(error)
 	})
 
 	instance.interceptors.response.use((config) => {
-		logger.debug('Received a response from BIN API')
+		logger.debug(BIN_API_RESPONSE_RECEIVED)
 		return config;
 	}, (error: Error) => {
-		logger.error('Error reading a BIN API response', { error })
+		logger.error(BIN_API_RESPONSE_ERROR, { error })
 		return Promise.reject(error)
 	});
 
---
+++ src/api/messages.ts
@@ -0,0 +1,4 @@
+export const BIN_API_REQUEST_SENT = 'BIN API: Request sent'
+export const BIN_API_REQUEST_ERROR = 'BIN API: Request error'
+export const BIN_API_RESPONSE_RECEIVED = 'BIN API: Response received'
+export const BIN_API_RESPONSE_ERROR = 'BIN API: Response error'
